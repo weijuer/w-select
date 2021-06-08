eecbb4ff7b460576fceec23b652ae0fd8e5518a0
Weijuer <ch_weijuer@163.com>
2021-06-08 15:08:28 +0800

    📦 1.去除无用代码，加入min、max数量限制

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,47 @@
 <template>
   <div id="app">
-    <form class="demo-form hidden" action>
+    <form action class="demo-form">
       <fieldset>
         <legend>单选</legend>
-        <w-select search :options="options" v-model="selected"></w-select>
+        <w-select :options="options" v-model="selected"></w-select>
+      </fieldset>
+
+      <fieldset>
+        <legend>单选-带搜索</legend>
+        <w-select :options="options" search v-model="selected"></w-select>
       </fieldset>
 
       <fieldset>
         <legend>多选</legend>
-        <w-select search multiple :options="options" v-model="selecteds"></w-select>
+        <w-select :options="options" v-model="selecteds"></w-select>
+      </fieldset>
+
+      <fieldset>
+        <legend>多选-带搜索</legend>
+        <w-select :options="options" multiple search v-model="selecteds"></w-select>
+      </fieldset>
+
+      <fieldset>
+        <legend>多选-带搜索+最小限制+最大限制</legend>
+        <w-select :max="3" :min="1" :options="options" @validate="validate" multiple search v-model="selecteds"></w-select>
       </fieldset>
     </form>
-
-    <simple-table :heroes="gridData" :columns="gridColumns" :filter-key="searchQuery"></simple-table>
   </div>
 </template>
 
 <script>
 import Select from './components/Select.vue'
-import SimpleTable from './components/SimpleTable.vue'
 
 export default {
   name: 'App',
   components: {
     [Select.name]: Select,
-    SimpleTable
   },
   data() {
     return {
       options: [],
       selected: '',
       selecteds: [],
-      searchQuery: '',
-      gridColumns: ['name', 'power'],
-      gridData: [
-        { name: 'Chuck Norris', power: Infinity },
-        { name: 'Bruce Lee', power: 9000 },
-        { name: 'Jackie Chan', power: 7000 },
-        { name: 'Jet Li', power: 8000 }
-      ]
     }
   },
   created() {
@@ -54,41 +57,48 @@ export default {
       return new Array(max).fill(0).map((option, index) => {
         return {
           text: 'Select-' + (index + 1),
-          key: 'key-' + (index + 1)
+          key: 'key-' + (index + 1),
         }
       })
-    }
-  }
+    },
+    validate(code) {
+      if (code === 'maxErr') {
+        console.log('max Err!')
+      }
+      if (code === 'minErr') {
+        console.log('min Err!')
+      }
+    },
+  },
 }
 </script>
 
 <style lang="stylus">
-html,
-body {
-  margin: 0
-  padding: 0
+html, body {
+    margin: 0;
+    padding: 0;
 }
 
 #app {
-  font-family: -apple-system, SF UI Text, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif, SimHei, SimSun
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
+    font-family: -apple-system, SF UI Text, Arial, PingFang SC, Hiragino Sans GB, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif, SimHei, SimSun;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
 .hidden {
-  display: none
+    display: none;
 }
 
 .demo-form {
-  margin: 0 auto
-  max-width: 960px
+    margin: 0 auto;
+    max-width: 960px;
 
-  fieldset {
-    margin-bottom: 1em
-    border: 1px solid #aaa
-  }
+    fieldset {
+        margin-bottom: 1em;
+        border: 1px solid #aaa;
+    }
 }
 </style>
